1d058394b14bfae07cffd452a3f6ba1bb1c9bdb9
DragonZX <dragonzx@yandex.ru>
2013-06-19 13:32:36 +0400

    Cleaning comments

--- parser.php
+++ parser.php
@@ -1,4 +1,4 @@
-<?php
+п»ї<?php
 session_start();
 //Icecast stats.xml parser
 include_once('channel.conf');
@@ -24,17 +24,16 @@ var $current = array();
             foreach ($this->xml->source as $arr) {
         
                 $mnt=str_replace("/","",$arr['mount']);
-                $this->stat[$mnt]['mount_name'] = $mnt; //ID маунта (например: ices)
-                $this->stat[$mnt]['audio_info'] = $arr->audio_info; //инфо маунта
-                $this->stat[$mnt]['bitrate'] = $arr->bitrate; //битрейт
-                $this->stat[$mnt]['genre'] = $arr->genre; //жанр    
-                //$this->stat[$mnt]['channels'] = $arr->ice-channels; //каналы (моно, стерео)
-                $this->stat[$mnt]['listener_peak'] = $arr->listener_peak; //пик слушателей
-                $this->stat[$mnt]['listeners'] = $arr->listeners; //текущее кол-во слушателей    
-                $this->stat[$mnt]['max_listeners'] = $arr->max_listeners; //максимум слушателей            
-                $this->stat[$mnt]['server_description'] = $arr->server_description; //описание маунта        
-                $this->stat[$mnt]['server_name'] = $arr->server_name; //название маунта
-                $this->stat[$mnt]['title'] = $arr->title; // название текущей песни
+                $this->stat[$mnt]['mount_name'] = $mnt;
+                $this->stat[$mnt]['audio_info'] = $arr->audio_info;
+                $this->stat[$mnt]['bitrate'] = $arr->bitrate;
+                $this->stat[$mnt]['genre'] = $arr->genre;
+                $this->stat[$mnt]['listener_peak'] = $arr->listener_peak;
+                $this->stat[$mnt]['listeners'] = $arr->listeners;   
+                $this->stat[$mnt]['max_listeners'] = $arr->max_listeners;           
+                $this->stat[$mnt]['server_description'] = $arr->server_description;       
+                $this->stat[$mnt]['server_name'] = $arr->server_name;
+                $this->stat[$mnt]['title'] = $arr->title;
                         
             }
             
@@ -57,43 +56,41 @@ var $current = array();
             
     }
     
-    /*function printcurrent() {
+    function printcurrent() {
     
-    //Выводимая инфа (+ диз если нужно). Можно дополнять и изменять 
+    //Output data (+ design if you need). You can change it as you wish
         print "
             <nowrap>
-            Активный маунт: ".$this->current['mount_name']."<br />
-            Название маунта: ".$this->current['server_name']."<br />
-            Описание маунта: ".$this->current['server_description']."<br />
-            Композиция: ".$this->current['title']."<br />";
+            Active mount: ".$this->current['mount_name']."<br />
+            Mount name: ".$this->current['server_name']."<br />
+            Mount description: ".$this->current['server_description']."<br />
+            Now playing: ".$this->current['title']."<br />";
     
         if (!$this->current['bitrate']) { 
 
         print "
-            Битрейт: 96 Кб/с<br />";            
+            Bitrate: 96 Kb/s<br />";            
         
         } else {
         
         print "
-            Битрейт: ".$this->current['bitrate']." Кб/с<br />";            
+            Bitrate: ".$this->current['bitrate']." Kb/s<br />";            
         
         }
 
         print "
-            Слушателей: ".$this->current['listeners']."<br />
-            Пик слушателей: ".$this->current['listener_peak']."
+            Listeners: ".$this->current['listeners']."<br />
+            Listeners peak: ".$this->current['listener_peak']."
             </nowrap>";
     
-    }*/
+    }
     
 }
 
 
-//Непосредственно вывод
+//Directly output
 
 $output = new Icestat;
-//заполнить данные
-//пример: $output->getfile('юзер', 'пароль', 'сервер', 'порт');
 $output->getfile($stream_admin, $stream_password, $stream_server, $stream_port);
 $output->parsefile();
 //$output->currentmount();
--- playing.php
+++ playing.php
@@ -1,12 +1,9 @@
 <?php
 include_once('channel.conf');
-//echo $date = date("Y-m-d H:i:s");echo "\n";
 echo $date = date("H:i");echo "\n";
 
 include_once('parser.php');
 $stat = new Icestat;
-//заполнить данные
-//пример: $output->getfile('юзер', 'пароль', 'сервер', 'порт');
 $stat->getfile($stream_admin, $stream_password, $stream_server, $stream_port);
 $stat->parsefile();
 $stat->currentmount();
@@ -15,12 +12,12 @@ $stat->currentmount();
   if ($stat->current['mount_name'] == $stream_nonstop){
 #echo 'mount point: <b>'.$point_name.'</b><br>';
 #echo 'm3u: <a href="'.$pount_m3u.'">'.$pount_m3u.'</a><br>';
-#echo 'Сейчас слушает: <b>'.$point_listners_count.'</b><br><br><br>';
-    echo '<br><b><font color="green">Сейчас Играет:</font></b><br><div style="margin-top:2px";><b>'. $stat->current['title'].'</b></div><br>';
+#echo 'Now listening: <b>'.$point_listners_count.'</b><br><br><br>';
+    echo '<br><b><font color="green">Now playing</font></b><br><div style="margin-top:2px";><b>'. $stat->current['title'].'</b></div><br>';
 	}
 	else
 	{
-	echo '<br><font color="red"><b>Прямой Эфир</b></font><br><div style="margin-top:2px";><font "color="blue"><b>'.$current_dj.'</b></font></div><div style="margin-top:2px";><b>'. $stat->current['title'].'</b></div><br>';
+	echo '<br><font color="red"><b>On air</b></font><br><div style="margin-top:2px";><font "color="blue"><b>'.$current_dj.'</b></font></div><div style="margin-top:2px";><b>'. $stat->current['title'].'</b></div><br>';
 	}
  
 ?>
